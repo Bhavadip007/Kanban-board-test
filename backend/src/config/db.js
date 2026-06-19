581115e002e4503f1c9a5e6583badc1f476d0ab6
Bhavadip <bhavadip@gvmtechnologies.com>
2026-06-19 13:32:44 +0530

    update db config logs

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const config = require('./index');
 
 const connectDB = async () => {
-  await mongoose.connect(config.mongodbUri);
+  try {
+    await mongoose.connect(config.mongodbUri);
+
+    console.log('✅ MongoDB Connected');
+  } catch (error) {
+    console.error('❌ MongoDB Connection Error:', error);
+
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
